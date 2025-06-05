f17ae99b566bb927fdb58af9ad289a27841fe1fd
Lonpsy <oolalonpe@gmail.com>
2025-06-05 19:06:26 +0200

    updated basic javascript
    updated basic javascript funtionalities without API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-
+function displayPoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem-display-position", {
+    strings: "This is a captivating poem .This is really captivating❤️❤️❤️",
+    autoStart: true,
+    cursor: "!",
+    delay: 100,
+  });
+}
+let formElement = document.querySelector("#form-element");
+formElement.addEventListener("submit", displayPoem);
